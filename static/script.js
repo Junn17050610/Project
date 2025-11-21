07af367e06ea24d6cd433074ce48d5dbde47a26d
Junn17050610 <cjuan61004@gmail.com>
2025-11-21 11:02:37 +0700

    test2_doang

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 
         // Konfigurasi
-        const API_URL = 'https://project-production-c482.up.railway.app/api/predict'; 
+        const API_URL = 'https://prediksi-cuaca.up.railway.app/api/predict'; 
 
         // Variabel global
         let selectedFile = null;
